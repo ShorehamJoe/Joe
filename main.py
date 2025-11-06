7effde3bdb621f929ba6b6855dd9bab40bf295c4
joer0 <joe.r02@outlook.com>
2025-11-06 10:02:20 +0000

    Adding functions

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Joe")
+
+
+def Word(first_name, last_name):
+    print(f"FORENAME: {first_name}\n SURNAME: {last_name}")
+
+
+Word("Joe", "Richards")
+Word("Jack", "Ryan")
+Word("Joe", "Richards")
